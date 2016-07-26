e92d12096aa08616aa192d2a8462700d387c70ac
unaibot <unaibot@gmail.com>
2016-07-26 13:02:08 +0200

    first commit

--- Shake_shack.rb
+++ Shake_shack.rb
@@ -1,1 +1,35 @@
-puts shake shack
+class Shake_Shop
+	def initialize
+
+
+  class Milkshake
+  	def initialize
+  		@base_price = 5
+  		@ingredients = [ ]
+  	end
+
+  class Ingredient 
+	attr_reader :name, :price
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
+
+	def add_milkshake(milkshake)
+		@milkshake.push(milkshake)
+
+
+  	def add_ingredient(ingredient)
+  		@ingredients.push(ingredient)
+  	end
+
+end
+
+nizars = milkshake = Milkshake.new
+banana = Ingredient.new("Banana", 2)
+chocolate_chips = Ingredient.new("Chocolate chips", 1)
+nizars_milkshake.add_ingredient(banana) 
+nizars_milkshake.add_ingredient(chocolate_chips)
+
+end
+
